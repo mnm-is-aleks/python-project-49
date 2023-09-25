b90bf0759a58413581d9e6282052b2d7d43f215e
user <user@Python.myguest.virtualbox.org>
2023-09-25 19:31:14 +0200

    add cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+import brain_games.cli as cli
+
+
 def main():
     print('Welcome to the Brain Games!')
     pass
 
 
+
 if __name__ == '__main__':
     main()
+    cli.welcome_user()
