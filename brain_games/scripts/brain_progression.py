c0dfd06a5adc3479e29e4913e9cdc54b65e40102
user <user@Python.myguest.virtualbox.org>
2023-09-28 12:32:05 +0200

    fixed lint brain-progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -8,7 +8,7 @@ def make_progression():
     n = int(choice('2345'))
     a = randint(1, 10)
     b = n * 10
-    ls = [i for i in range(a, b+a, n)]
+    ls = [i for i in range(a, b + a, n)]
     current = randint(1, 8)
     condition = ls[current]
     new_ls = [str(i) for i in ls]
